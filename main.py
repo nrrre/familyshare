f0f14b05fbfbefa6bf9128e086a926fbe72eafcd
nrrre <heyim9r@outlook.com>
2015-11-15 02:06:08 +0800

    you can upload the picture now!

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import MySQLdb.cursors
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 # import sae.const
+import os
+import time
+import re
+from flask import send_from_directory
 
 import sys
 reload(sys)
@@ -15,15 +19,18 @@ sys.setdefaultencoding('utf8')
 app = Flask(__name__)
 
 # Load default config and override config from an environment variable
+ALLOWED_EXTENSIONS = set(['png','jpg','jpeg','gif'])
+UPLOAD_FOLDER = 'uploads/'
 app.config.update(dict(
     DEBUG=True,
-    SECRET_KEY='hahah-2333'
+    SECRET_KEY='hahah-2333',
+    UPLOAD_FOLDER=UPLOAD_FOLDER
 ))
 MYSQL_DB = 'family'      # 数据库名
 MYSQL_USER = 'root'    # 用户名
 MYSQL_PASS = '2333'    # 密码
 MYSQL_HOST = 'localhost'    # 主库域名（可读写）
-MYSQL_PORT = 3306
+MYSQL_PORT = 3306 
 
 
 def connect_db():
@@ -49,14 +56,9 @@ def close_db(error):
         g.mysql_db.close()
 
 
-@app.route('/testt')
-def test():
-    db = get_db()
-    cur = db.cursor()
-    cur.execute('select * from testt')
-    create_dates = cur.fetchall()
-    print create_dates
-    return create_dates
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def show_entries():
@@ -67,6 +69,9 @@ def show_entries():
         entries = cur.fetchall()
 
         for entry in entries:
+            if re.findall(r'<IMG>(.+)</IMG>', entry['content']):             
+                entry['image'] = re.findall(r'<IMG>(.+)</IMG>', entry['content'])[0]
+                entry['content'] = re.findall(r'(.+)<IMG>', entry['content'])[0]
             cur.execute('select * from reply where share_id=%d order by id' % entry['id'])
             replys = cur.fetchall()
             if replys:
@@ -80,18 +85,43 @@ def show_entries():
     else:
         return redirect(url_for('login'))
 
+
+@app.route('/uploads/<filename>') 
+def uploaded_file(filename): 
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename) 
+
+
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
     cur = db.cursor()
-    cur.execute('insert into share (user_id, content) values ("%d", "%s")' % \
-               (int(session['user']),
-                request.form['text'].encode('utf8')))
-    db.commit()
-    flash('您的分享已经成功发布！')
+
+    if request.files['image']:
+        image = request.files['image']
+        if image and allowed_file(image.filename):
+            filename = time.strftime('%Y%m%d%H%M%S.', time.localtime()) + image.filename.split('.')[-1]
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        else:
+            flash('您上传的图片格式错误！请使用JPG/JPEG/PNG/GIF格式图片！')
+            return redirect(url_for('show_entries'))
+        media_type = 1
+        cur.execute('insert into share (user_id, content, media_type) values ("%d", "%s", "%d")' % \
+                (int(session['user']),
+                request.form['text'].encode('utf8') + '<IMG>' + filename + '</IMG>',
+                media_type))
+        db.commit()
+        flash('您的分享已经成功发布！')
+    else:
+        cur.execute('insert into share (user_id, content, media_type) values ("%d", "%s", 0)' % \
+            (int(session['user']),
+            request.form['text'].encode('utf8')))
+        db.commit()
+        flash('您的分享已经成功发布！')
+
     return redirect(url_for('show_entries'))
+
 
 @app.route('/reply/<int:share_id>', methods=['POST'])
 def reply(share_id):
@@ -107,13 +137,17 @@ def reply(share_id):
     flash('回复成功！')
     return redirect(url_for('show_entries'))
 
+
 @app.route('/del/<int:share_id>')
 def delete(share_id):
     db = get_db()
     cur = db.cursor()
-    cur.execute('select user_id from share where id = %d' % share_id)
+    cur.execute('select user_id, content, media_type from share where id = %d' % share_id)
     share = cur.fetchall()
     if session['user'] == int(share[0]['user_id']):
+        if share[0]['media_type']:
+            filename = re.findall(r'<IMG>(.+)</IMG>', share[0]['content'])[0]
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         cur.execute('delete from share where id = %d' % share_id)
         db.commit()
         cur.execute('delete from reply where share_id = %d' % share_id)
@@ -123,6 +157,7 @@ def delete(share_id):
     else:
         flash('您不能删除他人的分享！')
         return redirect(url_for('show_entries'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
